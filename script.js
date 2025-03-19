a3b5eb6f999495d345817055fcdbb3088627e0e4
granthyson <107725277+granthyson@users.noreply.github.com>
2025-03-19 14:20:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+      button.addEventListener('mouseenter', function() {
+        this.style.transform = 'translateY(-2px)';
+      });
+      
+      button.addEventListener('mouseleave', function() {
+        this.style.transform = 'translateY(0)';
+      });
+    });
+  
